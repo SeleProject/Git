4014c02122d54a063f36aad6569fb978f909a668
SeleProject <emilmariyans@gmail.com>
2024-05-17 16:49:42 +0530

    Asian development

--- src/module-info.java
+++ src/module-info.java
@@ -13,8 +13,18 @@ public class vowels {
 		
 			System.out.println(stringContainsVowels("Hello")); // true
 			System.out.println(stringContainsVowels("TV")); // false
+		
 			System.out.println(stringContainsVowels("12345678")); // new
 	}
+	public class vowels {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+				System.out.println(stringContainsVowels("tgtgtg")); // true
+				System.out.println(stringContainsVowels("uuuuu")); // false
+				System.out.println(stringContainsVowels("77777")); // new
+		}
 module Hello {
 	System.out.println("hii");
 }
